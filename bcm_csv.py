75e413a971c54ad0f6960cd82703d7babd058353
cjohnson <chris@betterdataadvisors.com>
2021-12-27 20:43:15 -0600

    removed marker_colors

--- bcm_csv.py
+++ bcm_csv.py
@@ -10,7 +10,6 @@ fig = go.Figure(go.Treemap(
         ids = df.ids,
         labels = df.labels,
         parents = df.parents,
-        #marker_colorscale = 'Blues'))
         marker_colors = ["pink", "royalblue", "lightgray", "purple", "cyan", "lightgray", "lightblue"]))
 #fig.write_html("bcm.html")
 fig.show()
